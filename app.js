ae8bf3aa967d4c967527f9e47fa02167c3de120d
Rida Riaz <ridariaz@Ridas-MacBook-Pro.local>
2022-09-16 15:44:45 -0700

    app.js updated

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // Step 1: import the data from data.js
 const tableData = data;
 // D3: JavaScript lib that produces sophistactaed and highly dynamic graphisc in an HTML webpage 
+
 // Step 2: point our data to our HTML page 
 // Reference the HTML table using d3
 var tbody = d3.select("tboday");
     // declare a variable, tbody
     // use d3.select to tell JS to look for the <tbody> tags in the html
+
 // Step 3: bulding the table 
 // need to clean the table first to avoid creating any duplicates with the existing data 
 function buildTable(data) {
@@ -27,3 +29,30 @@ function buildTable(data) {
       );
     });
 }
+
+// Step 4: Creating a handleClick function bc it will be handling what to do after an input is given, such as filtering the table by date
+// adding a date fxn, creating a couple of variables to hold our date data (both filtered and unfiltered)
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+  
+     // Check to see if a date was entered and filter the
+    // data using that date.
+    if (date) {
+      // Apply `filter` to the table data to only keep the
+      // rows where the `datetime` value matches the filter value
+      filteredData = filteredData.filter(row => row.datetime === date);
+    }
+  
+     // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+  }
+  
+  // Attach an event to listen for the form button
+  d3.selectAll("#filter-btn").on("click", handleClick);
+  
+  // Build the table when the page loads
+  buildTable(tableData);
